351156076ca0e52811e4b3b0bc56985f42a2a627
phellyokesh <gesarephelisters2@gmail.com>
2026-03-18 16:18:42 +0300

    Add files via upload

---
+++ first tutorial.py
@@ -0,0 +1,10 @@
+def get_choices():
+    player_choice = "rock"
+    computer_choice = "paper"
+
+    return computer_choice
+
+def greeting():
+    return "Hi" 
+response=greeting()
+print(response)
